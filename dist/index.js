15fbbd21636be9ef6a3982ee001e3646b58786a3
Karan Popat <karan@turbot.com>
2022-09-28 21:36:40 +0530

    infra scan

--- dist/index.js
+++ dist/index.js
@@ -13843,7 +13843,7 @@ async function run() {
             await (0, steampipe_1.runSteampipeCheck)(steampipePath, modPath, inputs, ["json", "md"]);
         }
         catch (e) {
-            // throw e
+            throw e;
         }
         finally {
             await exportStepSummary(inputs);
